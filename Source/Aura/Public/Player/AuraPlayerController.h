08369547b255453cf62b96e5f35a021fd99cf96d
Anton Dessov <90501686+antondessov@users.noreply.github.com>
2023-08-08 15:23:50 +0300

    Aura Player Controller

--- Source/Aura/Public/Player/AuraPlayerController.h
+++ Source/Aura/Public/Player/AuraPlayerController.h
@@ -6,12 +6,20 @@
 #include "GameFramework/PlayerController.h"
 #include "AuraPlayerController.generated.h"
 
-/**
- * 
- */
+
+class UInputMappingContext;
+
 UCLASS()
 class AURA_API AAuraPlayerController : public APlayerController
 {
 	GENERATED_BODY()
+public:
+	AAuraPlayerController();
 	
+protected:
+	virtual void BeginPlay();
+	
+private:
+	UPROPERTY(EditAnywhere, Category = "")
+	TObjectPtr<UInputMappingContext> AuraContext;
 };
