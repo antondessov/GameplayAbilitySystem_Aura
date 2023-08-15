839f10c85dc9c45412b53002a7bc1e6da1ca3205
Anton Dessov <90501686+antondessov@users.noreply.github.com>
2023-08-15 15:09:25 +0300

    Aura HUD 2

---
+++ Source/Aura/Private/UI/HUD/AuraHUD.cpp
@@ -0,0 +1,13 @@
+// Copyright Comrades
+
+
+#include "UI/HUD/AuraHUD.h"
+#include "UI/Widget/AuraUserWidget.h"
+
+void AAuraHUD::BeginPlay()
+{
+	Super::BeginPlay();
+
+	UUserWidget* Widget = CreateWidget<UUserWidget>(GetWorld(), OverlayWidgetClass);
+	Widget->AddToViewport();
+}
