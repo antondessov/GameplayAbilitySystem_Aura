dfe5d818b1943c139193fffb9588dfe7b2b80a38
Anton Dessov <90501686+antondessov@users.noreply.github.com>
2024-06-27 14:34:37 +0300

    FireBlast

--- Source/Aura/Public/AuraGameplayTags.h
+++ Source/Aura/Public/AuraGameplayTags.h
@@ -81,6 +81,7 @@ static const FAuraGameplayTags& Get() {return GameplayTags;}
  
  
  FGameplayTag Abilities_Fire_FireBolt;
+ FGameplayTag Abilities_Fire_FireBlast;
  FGameplayTag Abilities_Lightning_Electrocute;
  FGameplayTag Abilities_Arcane_ArcaneShards;
 
